6041003324fb5bdff3938d95300fcaec3d2931b3
Dawid Maciak <maciak.dawid.96@gmail.com>
2024-11-16 11:18:47 +0100

    Init Project

---
+++ src/main/java/pl/daveproject/DietAppCliApplication.java
@@ -0,0 +1,9 @@
+package pl.daveproject;
+
+public class DietAppCliApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/pl/daveproject/DietAppCliApplicationTest.java
@@ -0,0 +1,38 @@
+package pl.daveproject;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class DietAppCliApplicationTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public DietAppCliApplicationTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( DietAppCliApplicationTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
